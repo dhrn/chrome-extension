4e4c521a2d1abefee8b6dd8e741b12a7b77183af
dhrn <dharangkrr@gmail.com>
2024-03-31 16:35:45 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+declare let ComlinkWorker: any; 
+
+
+const workerInstance = new ComlinkWorker(new URL("./worker/fib", import.meta.url))
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const onClick = async () => {
+    setCount((c) => c+1)
+    const result = await workerInstance.fibonacci(count+1)
+    console.log(count+1, result)
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => onClick()}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
